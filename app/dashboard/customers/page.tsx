0793db4edaaf505d700c8eaecdaf1ee3c4af28da
Asteises <p.nikitaev@gmail.com>
2024-02-17 18:40:05 +0300

    add nav

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,3 +1,9 @@
 export default function Page() {
-    return <p>Customers Page</p>;
-  }
+  return (
+    <main>
+      <h1>CUSTOMERS</h1>
+      <p>Customers Page</p>
+      <p>1</p>
+    </main>
+  );
+}
